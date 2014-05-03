fd145b4ab28d5ea21eca91ac3b980ed234739f2a
nperez0111 <nperez0111@gmail.com>
2014-05-03 00:34:51 -0400

    Start page FS members resized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+
+$( document ).ready(function() {
+$('li').click(function(e){
+	e.preventDefault();
+	console.log($(this).find("href").attr('id'));
+	$($(this).children().attr('href')).velocity("scroll", 1000)
+  .velocity({ opacity: 1 });
+});
+	var theWindow        = $(window),
+	    $bg              = $(".container-fluid"),
+	    aspectRatio      = $bg.width() / $bg.height();
+	    
+	    			    		
+	function resizeBg() {
+		h= theWindow.height()+"px";
+		hs= (theWindow.height()/2)-($bg.children().height()/2);
+		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+		    $bg
+		    	.css({"height":"100%","width":"auto"
+		    	});
+		    	$bg.children().css("margin-top", hs);
+		    	$('nav').css("margin-top",h);
+		} else {
+		    $bg
+		    	.css({"width":"100%","height":"auto"		
+		    });$bg.children().css("margin-top", hs);
+		    	$('nav').css("margin-top",h);
+		}
+					
+	}
+	                   			
+	theWindow.resize(resizeBg).trigger("resize");
+
+});
